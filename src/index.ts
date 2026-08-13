d93854f30ac6cddfc9fc4b45322e5ba6c68e2f62
Marc Campbell <marc.e.campbell@gmail.com>
2026-08-13 11:27:44 -0700

    Publish Helm as a standalone runtime

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 #!/usr/bin/env node
 
+import { realpath } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
 import { Adversary } from "@adversarylabs/sdk";
 import { analyzeRepository } from "./analyze.js";
 import { registerRules } from "./rules.js";
 
 export function createApp(): Adversary {
-  const app = new Adversary({ name: "helm", version: "0.0.12", review: { maximumFindings: 8 } });
+  const app = new Adversary({ name: "helm", version: "0.0.13", review: { maximumFindings: 8 } });
   registerRules(app);
   app.rule("helm.review", async (ctx) => analyzeRepository(ctx));
   return app;
 }
 
-if (process.argv[1] !== undefined && import.meta.url === new URL(process.argv[1], "file:").href) {
+if (
+  process.argv[1] !== undefined &&
+  (await realpath(process.argv[1])) === (await realpath(fileURLToPath(import.meta.url)))
+) {
   await createApp().runFromEnvironment();
 }
